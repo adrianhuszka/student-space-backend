a0c5eaf12363e5e3efc1e8ad5a2176f9c90c5ea9
Huszka Adrián Gábor <adrian.huszka@gmail.com>
2024-02-23 00:22:08 +0100

    Spring Security config

--- user-service/src/main/java/hu/StudentSpace/utils/JwtDecoder.java
+++ user-service/src/main/java/hu/StudentSpace/utils/JwtDecoder.java
@@ -1,2 +1,14 @@
-package hu.StudentSpace.utils;public class JwtDecoder {
+package hu.StudentSpace.utils;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class JwtDecoder {
+    public String decode(String token) {
+        String[] split_string = token.split("\\.");
+        String base64EncodedBody = split_string[1];
+
+        Base64 base64Url = new Base64(true);
+
+        return new String(base64Url.decode(base64EncodedBody));
+    }
 }
